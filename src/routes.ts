3fa6f40c3255999d15afe822fa360d7452069a9e
Vitória Camila <vcamilaxs@gmail.com>
2020-06-29 17:20:46 -0300

    Dia 2 - Criando Rotas

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,47 @@
 import express from 'express';
+import knex from './database/connection'
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-    return res.json();
+routes.get('/items', async (req, res) => {
+    const items = await knex('items').select('*');
+
+    // Serialize serve pra organizar os dados recebidos do BD
+    const serializedItems = items.map(item => {
+        return { 
+            id: item.id,
+            name: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`
+        };
+    });
+    return res.json(serializedItems);
+});
+
+routes.post('/points', async (req, res) => {
+    const {
+        name,
+        email,
+        whatsapp,
+        latitude, 
+        longitute,
+        city,
+        uf,
+        items
+    } = req.body;
+
+    knex('points').insert({
+        image: 'image-fake',
+        name,
+        email, 
+        whatsapp,
+        latitude,
+        longitute,
+        city, 
+        uf,
+        items
+    });
+
+    return res.json({ sucess: true });
 });
 
 export default routes;
